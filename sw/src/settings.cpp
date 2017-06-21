5da1f193594411fc879f6458c918c28837bf789c
andy <andy@skyrain.eu>
2017-06-21 16:17:29 +0200

    feat(config): add support for user settings and ini files reading

---
+++ sw/src/settings.cpp
@@ -0,0 +1,114 @@
+#include "common.h"
+#include <sys/stat.h>
+#include "SimpleIni.h"
+
+#include "settings.h"
+
+static char defCfgFile[] = CFG_FILE;
+static char defLogFile[] = LOG_FILE;
+
+struct stSettings settings = {
+    defCfgFile,
+    defLogFile,
+    LOG_LVL_SYSTEM,
+    LOG_LVL_FILE,
+    {
+        (char *)GPRS_APN,
+        (char *)GPRS_USERNAME,
+        (char *)GPRS_PASSWORD
+    },
+    {
+        (char *)SERVER_URL,
+        SERVER_PORT,
+        (char *)SERVER_PATH
+    },
+    {
+        (char *)DEF_SERIAL_NODE,
+        SIM900_BAUDRATE,
+        (char *)SIM_PIN
+    }
+};
+
+bool LoadSettingsString(CSimpleIniA *pIni, char **pConfig, const char *pSectionName, const char *pKeyName) {
+    const char *pStr;
+    pStr = pIni->GetValue(pSectionName, pKeyName, *pConfig);
+    if (pStr) {
+        *pConfig = strdup(pStr);
+        if (*pConfig == NULL) {
+            CLogger::GetLogger()->LogPrintf(LL_ERROR, "Allocation of memory for key '%s' failed!", pKeyName);
+            return false;
+        }
+    }
+    return true;
+}
+
+int LoadSettings(const char *fileName) {
+    // check if file name is not null
+    if (fileName == NULL) {
+        CLogger::GetLogger()->LogPrintf(LL_ERROR, "Configuration file is null!");
+        return -1;
+
+    }
+    // check if image exist with r rights
+    if (!access(fileName, R_OK)) {
+        CLogger::GetLogger()->LogPrintf(LL_ERROR, "Configuration file is not readable!");
+        return -1;
+    }
+
+    SI_Error ret;
+
+    // read settings from file regarding to iniSettingsDesc descriptor
+    CSimpleIniA ini;
+    if ((ret = ini.LoadFile(fileName)) < 0) {
+        CLogger::GetLogger()->LogPrintf(LL_ERROR, "Configuration file %s reading failed with error: %i!", fileName, ret);
+        return ret;
+    }
+
+    // load log file
+    LoadSettingsString(&ini, &settings.pLogFileName, SECTION_INI_GENERAL, "LogFileName");
+    settings.logLevelFile = static_cast<LogLevel>(ini.GetLongValue(SECTION_INI_GENERAL, "LogFileLevel", LOG_LVL_FILE));
+    // enable logging into file
+    if (!CLogger::GetLogger()->OpenLogFile(settings.logLevelFile, settings.pLogFileName)) {
+        return SI_FILE;
+    }
+
+    // gprs parameters
+    LoadSettingsString(&ini, &settings.GPRS.apn, SECTION_INI_GPRS, "apn");
+    LoadSettingsString(&ini, &settings.GPRS.username, SECTION_INI_GPRS, "username");
+    LoadSettingsString(&ini, &settings.GPRS.password, SECTION_INI_GPRS, "password");
+
+    // server settings
+    LoadSettingsString(&ini, &settings.server.url, SECTION_INI_SERVER, "server");
+    settings.server.port = ini.GetLongValue(SECTION_INI_SERVER, "port", SERVER_PORT);
+    LoadSettingsString(&ini, &settings.server.path, SECTION_INI_SERVER, "path");
+
+    // module settings
+    LoadSettingsString(&ini, &settings.module.node, SECTION_INI_MODULE, "device");
+    settings.module.baudrate = static_cast<BaudRate>(ini.GetLongValue(SECTION_INI_MODULE, "baudrate", SIM900_BAUDRATE));
+    LoadSettingsString(&ini, &settings.module.pin, SECTION_INI_MODULE, "pin");
+
+    CLogger::GetLogger()->LogPrintf(LL_DEBUG, "Parameters have been successfully loaded!");
+
+    return ret;
+}
+
+void UnloadSettings(void) {
+
+    // check cfg file
+    if (settings.pCfgFileName != NULL)
+        free(settings.pCfgFileName);
+
+    // check log file
+    if (settings.pLogFileName != NULL)
+        free(settings.pLogFileName);
+
+    // TODO: need test
+    if (&settings.GPRS != NULL) {
+        for (int i=0;i<3;i++) {
+            char *tmp = (char *)(settings.GPRS.apn) + i*(sizeof(char));
+            if (tmp != NULL) {
+                free(tmp);
+            }
+        }
+    }
+}
---
+++ sw/src/settings.h
@@ -0,0 +1,74 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+#include "serial.h"
+#include "logger.h"
+
+// configuration file location
+#define CFG_FILE "/opt/cam-system/cam-system.cfg"
+// log file location
+#define LOG_FILE "/opt/tmp/cam-system/file.log"
+// system log level
+#define LOG_LVL_SYSTEM LL_DEBUG
+// file log level
+#define LOG_LVL_FILE LL_INFO
+
+// gprs settings
+#define GPRS_APN        ""
+#define GPRS_USERNAME   ""
+#define GPRS_PASSWORD   ""
+
+// server settings
+#define SERVER_URL  "google.com"
+#define SERVER_PORT 80
+#define SERVER_PATH "/"
+
+// sim settings
+#define SIM_PIN ""
+
+// sim900 settings
+#define SIM900_BAUDRATE BR_9600
+
+// system settings
+#define SYS_NAME "RPI_cam-system"
+
+// ini file section
+#define SECTION_INI_GENERAL "general"
+#define SECTION_INI_GPRS    "gprs"
+#define SECTION_INI_SERVER  "server"
+#define SECTION_INI_MODULE  "module"
+
+struct stSetGPRS {
+    char *apn;
+    char *username;
+    char *password;
+};
+
+struct stSetServer {
+    char *url;
+    int port;
+    char *path;
+};
+
+struct stSetModule {
+    char *node;
+    BaudRate baudrate;
+    char *pin;
+};
+
+struct stSettings {
+    char *pCfgFileName;
+    char *pLogFileName;
+    LogLevel logLevelFile;
+    LogLevel logLevelSystem;
+    stSetGPRS GPRS;
+    stSetServer server;
+    stSetModule module;
+};
+
+extern struct stSettings settings;
+
+int LoadSettings(const char *fileName);
+void UnloadSettings(void);
+
+#endif // SETTINGS_H
